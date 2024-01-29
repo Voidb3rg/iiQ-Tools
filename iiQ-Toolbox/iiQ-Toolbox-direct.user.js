7497045a21a208847ef85ebe57f8598c6d3be959
Voidb3rg <32471640+Voidb3rg@users.noreply.github.com>
2024-01-29 22:11:04 +0100

    1.1.1

--- iiQ-Toolbox/iiQ-Toolbox-direct.user.js
+++ iiQ-Toolbox/iiQ-Toolbox-direct.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         iiQ-Toolbox
 // @namespace    none
-// @version      1.1
+// @version      1.1.1
 // @description  I just did it because I'm bored... How you will use this menu depends on you! I did it for the developers of cheats on Moo Moo.io, but it can be used on any game.
 // @author       00100110#6361
 // @match        https://app.iiq-check.de/*
 // @match        *://moomoo.io/*
 // @grant        none
-// @downloadURL https://github.com/Voidb3rg/iiQ-Tools/blob/main/iiQ-Toolbox/iiQ-Toolbox-direct.user.js
-// @updateURL https://github.com/Voidb3rg/iiQ-Tools/blob/main/iiQ-Toolbox/iiQ-Toolbox-direct.user.js
+// @downloadURL https://github.com/Voidb3rg/iiQ-Tools/raw/main/iiQ-Toolbox/iiQ-Toolbox-direct.user.js
+// @updateURL https://github.com/Voidb3rg/iiQ-Tools/raw/main/iiQ-Toolbox/iiQ-Toolbox-direct.user.js
 // ==/UserScript==
 
 /* Setting menu */
@@ -87,7 +87,7 @@ class="" & id="" - I use to denote blocks, id for everything else
     <main class="menu--inner-gui">
       <passive class="menu--inner-gui-block">
         <passive id="menu--inner-gui-block-text">
-        <form><label for="Kundennummer">Kundennummer:</label><input type="form" id="Kundennummer"></form>
+        <form><label for="Kundennummer">Kundennummer:</label><input type="form" style="text-color:black" id="Kundennummer"></form>
         </passive>
 
       </passive>
